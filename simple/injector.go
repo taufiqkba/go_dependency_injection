6671bb33cf9e0658030567032a93ab29fea4f3a5
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-12 10:56:46 +0700

    Create injector

--- simple/injector.go
+++ simple/injector.go
@@ -2,3 +2,12 @@
 // +build wireinject
 
 package simple
+
+import "github.com/google/wire"
+
+func InitializedService() *SimpleService {
+	wire.Build(
+		NewSimpleRepository, NewSimpleService,
+	)
+	return nil
+}
